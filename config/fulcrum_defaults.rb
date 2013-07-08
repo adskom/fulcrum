bb6abb8203ecb0bdc98c772b4d92708005e93bff
Daniel Armanto <armanto@gmail.com>
2013-07-08 13:44:27 +0700

    Add smtp settings

--- config/fulcrum_defaults.rb
+++ config/fulcrum_defaults.rb
@@ -1,10 +1,12 @@
 Configuration.for('fulcrum') do
   # On Heroku, ensure you `heroku config:add APP_HOST=appname.herokuapp.com`
-  app_host ENV['APP_HOST'] || '127.0.0.1:3000'
+  app_host ENV['APP_HOST'] || 'pivotal.adskom.net'
 
   # The address which system emails will originate from.
-  mailer_sender ENV['MAILER_SENDER'] || 'noreply@example.com'
+  mailer_sender ENV['MAILER_SENDER'] || 'noreply@adskom.net'
 
   # Disable registration pages
   disable_registration ENV['DISABLE_REGISTRATION'] || false
+
+  smtp_password 'abcd'
 end
